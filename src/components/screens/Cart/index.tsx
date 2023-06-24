388bfeed6cd7f29b07431febd92ee3cd80d81411
darylaranha <daryl.aranha@gmail.com>
2023-06-24 03:34:24 -0400

    Screens added

---
+++ src/components/screens/Cart/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export function Cart(): JSX.Element {
+  return (
+    <View>
+      <Text>Cart Page</Text>
+    </View>
+  );
+}
---
+++ src/components/screens/Discover/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+export function Discover(): JSX.Element {
+  return (
+    <View>
+      <Text>Discover Page</Text>
+    </View>
+  );
+}
